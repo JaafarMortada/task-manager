b6f444a884a715526a09db1d90213904d3ecded5
Jaafar Mortada <jaafar1899@outlook.com>
2023-12-20 22:30:23 +0200

    frontend <> adding task card & filtering

---
+++ frontend/src/components/cards/TaskCard.tsx
@@ -0,0 +1,59 @@
+// import PropTypes from 'prop-types'
+import { Card, CardHeader, CardBody, Typography } from "@material-tailwind/react";
+import moment from 'moment'
+
+interface TaskCardProps {
+    task: any
+}
+
+
+const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
+
+    const date = moment(task.date);
+    const color = task.stage === 100 ? 'green' : (task.stage < 100 && task.stage > 0) ? 'yellow' : 'red'
+    return (
+        <Card
+        placeholder={undefined}
+            color="white"
+            className={` max-w-[calc(100%-20px)]  py-4 z-[1] px-5 rounded-none shadow-sm flex flex-row border-l-[5px] border-${color}-500 `}
+        >
+            <CardHeader
+            placeholder={undefined}
+                color="transparent"
+                floated={false}
+                shadow={false}
+                className="mx-0 z-[0] flex gap-4 items-start  min-w-[100px] pb-8 "
+            >
+                <div className="flex min-w-max bg-gray-500 flex-col  gap-0.5 rounded-full h-20 w-20 items-center justify-center">
+
+                    <div className="flex items-center justify-between">
+                        <Typography variant="h5" className={`text-gray-100 text-[14px]`} placeholder={undefined}>
+                            {date.format('dddd')} 
+                        </Typography>
+
+                    </div>
+                    <Typography className={` font-extrabold text-gray-100 text-[18px] `} placeholder={undefined}>{date.format('Do')}</Typography>
+                </div>
+
+            </CardHeader>
+            <CardBody placeholder={undefined} className="z-[0] mb-6 p-0 flex flex-col gap-3">
+                <div className={`flex min-w-max flex-col gap-0.5`}>
+
+                    <div className={`flex items-center justify-between `}>
+                        <Typography variant="h6" className="text-[--gray]" placeholder={undefined}>
+                            {date.format('dddd, MMMM Do, YYYY')}
+                        </Typography>
+
+                    </div>
+                    <Typography className=" text-left font-bold text-[21px]" placeholder={undefined}>{task.title}</Typography>
+                </div>
+                <Typography className={` text-justify`} placeholder={undefined}>
+                    {task.notes}
+                </Typography>
+            </CardBody>
+        </Card>
+
+    ) 
+}
+
+export default TaskCard
--- frontend/src/pages/TasksPage.tsx
+++ frontend/src/pages/TasksPage.tsx
@@ -1,41 +1,129 @@
-import { Typography } from "@material-tailwind/react"
 import { StickyNavbar } from "../components/ui/Navbar"
+import TaskCard from "../components/cards/TaskCard"
+import CustomSelect from "../components/ui/CustomSelect";
+import { useState } from "react";
+import CustomButton from "../components/ui/CustomButton";
+
+const dummyData = [
+    {
+        id: 1,
+        date: '2023-12-25T12:00:00Z',
+        title: 'Plan Holiday Celebration',
+        notes: 'Prepare for a joyful holiday celebration with friends and family.',
+        stage: 100,
+    },
+    {
+        id: 2,
+        date: '2023-12-30T10:30:00Z',
+        title: 'Meeting with Clients',
+        notes: 'Discuss upcoming projects and finalize client requirements.',
+        stage: 50,
+    },
+    {
+        id: 3,
+        date: '2023-12-30T10:30:00Z',
+        title: 'Meeting with Clients',
+        notes: 'Discuss upcoming projects and finalize client requirements.',
+        stage: 0,
+
+    },
+    {
+        id: 4,
+        date: '2023-12-30T10:30:00Z',
+        title: 'Meeting with Clients',
+        notes: 'Discuss upcoming projects and finalize client requirements.',
+        stage: 50,
+
+    },
+    {
+        id: 5,
+        date: '2023-12-30T10:30:00Z',
+        title: 'Meeting with Clients',
+        notes: 'Discuss upcoming projects and finalize client requirements.',
+        stage: 50,
+
+    },
+    {
+        id: 6,
+        date: '2023-12-30T10:30:00Z',
+        title: 'Meeting with Clients',
+        notes: 'Discuss upcoming projects and finalize client requirements.',
+        stage: 100,
+
+    },
+    {
+        id: 7,
+        date: '2023-12-30T10:30:00Z',
+        title: 'Meeting with Clients',
+        notes: 'Discuss upcoming projects and finalize client requirements.',
+        stage: 0,
+
+    },
+    {
+        id: 8,
+        date: '2023-12-30T10:30:00Z',
+        title: 'Meeting with Clients',
+        notes: 'Discuss upcoming projects and finalize client requirements.',
+        stage: 50,
+
+    },
+    {
+        id: 9,
+        date: '2023-12-30T10:30:00Z',
+        title: 'Meeting with Clients',
+        notes: 'Discuss upcoming projects and finalize client requirements.',
+        stage: 100,
+
+    },
+];
 
 const TasksPage = () => {
-  return (
-    <div className="flex flex-col items-center">
-        <StickyNavbar />
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-        <Typography variant="h1" placeholder={undefined} >test</Typography>
-    </div>
-  )
+
+    const [tasks, setTasks] = useState(dummyData)
+
+    const taskFilteringHandler = (filter: "all" | "completed" | "incomplete") => {
+        switch (filter) {
+            case "all":
+                setTasks(dummyData)
+                break;
+            case "completed":
+                setTasks(dummyData.filter(task => task.stage === 100))
+                break;
+            case "incomplete":
+                setTasks(dummyData.filter(task => task.stage > 0 && task.stage < 100))
+                break;
+            default:
+                setTasks(dummyData)
+                break;
+        }
+    }
+
+    return (
+        <div className="flex flex-col items-center">
+            <StickyNavbar />
+            <div className="z-[998] flex sm:flex-row flex-col sm:justify-between justify-center gap-5 items-center py-4 sticky top-[61px] lg:top-[68px] max-w-[calc(100%-20px)] min-w-[calc(100%-20px)] rounded-b-md purple__glass px-4 lg:px-8 lg:py-3 ">
+                <CustomSelect
+                    label={"Filter tasks"}
+                    options={[
+                        { value: "all", label: "All" },
+                        { value: "completed", label: "Completed" },
+                        { value: "incomplete", label: "Incomplete" },
+                    ]}
+                    onChange={(e) => {
+                        taskFilteringHandler(e as any)}}
+                    value={"all"}
+                    classNames=" w-[250px] z-[999]"
+                />
+                <CustomButton label="Add Task" containerClassNames="w-[220px]"/>
+            </div>
+            <div className="flex flex-col z-[2] gap-2 mt-2 items-center mb-5">
+                {tasks.map((task) => (
+                    <TaskCard key={task.id} task={task} />
+                ))}
+            </div>
+
+        </div>
+    )
 }
 
 export default TasksPage
